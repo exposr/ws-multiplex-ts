4a5890df851f84cf775a3ff1d4abe6cdf49423ea
Fredrik Lindberg <fli@fli.dev>
2023-08-31 22:02:52 +0200

    Export WebSocketMultiplexOptions

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { WebSocketMultiplex } from "./ws-multiplex";
 export { WebSocketMultiplex } from "./ws-multiplex";
 export { WebSocketMultiplexError, WebSocketMultiplexErrorCode } from "./ws-multiplex-error";
 export { WebSocketMultiplexSocket } from "./ws-multiplex-socket";
+export { WebSocketMultiplexOptions } from "./ws-multiplex";
 
 export default WebSocketMultiplex;
